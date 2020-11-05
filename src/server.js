6c18e7b933b7791935ece8403b8a21c0d5ee0c6f
pedrohenriquebraga <gamesantos7@gmail.com>
2020-11-05 11:14:41 -0300

    Separe the css and add new pages

--- src/server.js
+++ src/server.js
@@ -14,12 +14,29 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 
 // Rota Principal
 app.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/views/index.html')
+    return res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 // Rota para mostrar comandos
 app.get('/commands', (req, res) => {
-    return res.sendFile(__dirname + '/views/commands.html')
+    return res.sendFile(path.join(__dirname, 'views', 'commands.html'))
+})
+
+// Convite da Lilly
+app.get('/invite', (req, res) => {
+    return res.redirect('https://discord.com/api/oauth2/authorize?client_id=754548334328283137&permissions=8&scope=bot')
+})
+
+app.get('/support', (req, res) => {
+    return res.redirect('https://discord.gg/SceHNfZ')
+})
+
+app.get('/community-terms', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'communityTerms.html'))
+})
+
+app.get('/privacy-policy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'privacyPolicy.html'))
 })
 
 app.listen(process.env.PORT || 3000)
